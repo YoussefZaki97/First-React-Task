94a052eb3bd4e2915326cbbe9e59da638ed4a50b
YoussefZaki97 <youssefzaki@live.com>
2018-10-26 19:11:23 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+class App extends Component
+{
+  constructor(props) {
+    super(props);
+    this.state = {value: null,log:false};  //the color value that will be added to the style
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.stateToggle = this.stateToggle.bind(this);
+  }
+
+  temp={ val :null};          //dummy
+  handleChange(event) {       //store the user input onchange
+    this.temp.val = event.target.value;
+  }
+
+  handleSubmit(event) {       //use the input after submitting
+    this.setState({value: this.temp.val});
+    this.temp.val = null;     //return to null for new values to be added
+    event.preventDefault();
+  }
+
+  stateToggle=()=>{
+    this.setState ((state,props)=>
+  ({
+    log : !state.log
+  })
+)}
+
+  render()
+  {
+    return<div>
+     {this.state.log?<form>
+        <label>
+          Your Required Color:
+          <input type="text" value={this.temp.val} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" onClick={this.handleSubmit}/>
+        <p style={{color:this.state.value}}>Watch This</p>
+
+      </form>:<h1>Toggle Please</h1>}
+      <button onClick={this.stateToggle}>Toggle</button>
+    </div>
   }
 }
+
 
 export default App;
